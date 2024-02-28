6ed3353248e53cb5811436c179c29c5b48bc8c96
richardtag <richard@techaffinity.com>
2024-02-28 11:59:34 +0200

    New Redirect Link

--- pages/successpay.js
+++ pages/successpay.js
@@ -13,7 +13,7 @@ const SuccessPay = () => {
     <div className='w-100 my-5 d-flex justify-content-center align-items-center'>
       <div className='bg-white d-flex flex-column align-items-center shadow px-5 py-3'>
         <Image1 src={Success} width="80" height="80" />
-        <h5 className='text-center my-4 text-success'>Payment Failed</h5>
+        <h5 className='text-center my-4 text-success'>Payment Succeded</h5>
         <p className='text-center'><small className='text-muted'> Thanks for yout commitment to our platform. <br /> It's always great time with us.</small></p>
         <Link className='btn btn-success rounded w-100 mb-3' href={'/'}>Done</Link>
       </div>
